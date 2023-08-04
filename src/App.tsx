131593003957f9b5397ffffdf06e84c96bf09c62
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-08-03 20:18:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Search from './Components/SearchBar';
+import { Cat } from './types';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [breeds, setBreeds] = useState<Cat[]>([]);
 
-    return (
-        <>
-            <div className="flex justify-center">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <h1 className="font-bold underline bg-red-500 italic text-re">
-                Test header
-            </h1>
-        </>
-    );
+    async function getBreeds() {
+        const response = await fetch(
+            'https://cat-wiki-ou88.onrender.com/breeds',
+        );
+        const json: Cat[] = await response.json();
+        setBreeds(json);
+        console.log('information fetched');
+    }
+
+    useEffect(() => {
+        getBreeds();
+    }, []);
+
+    return <Search breeds={breeds} />;
 }
 
 export default App;
