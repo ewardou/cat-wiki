9d3f70a0f0bcd428219e0cd396c14b952c8d7c97
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-08-03 23:18:07 -0400

    Create a modal for SearchBar

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -3,19 +3,24 @@ import { useState } from 'react';
 
 export default function Search({ breeds }: { breeds: Cat[] }) {
     const [inputValue, setInputValue] = useState<string>('');
+    const [modalMsg, setModalMsg] = useState<string>('');
 
     const options = breeds.map((breed) => (
         <option value={breed.name} key={breed.id} />
     ));
 
+    function addTimeout(msg: string) {
+        setModalMsg(msg);
+        setTimeout(() => setModalMsg(''), 2500);
+    }
+
     function checkName() {
+        if (!inputValue) return addTimeout('Please enter a breed name');
         const obj = breeds.find((item) =>
             item.name.toLowerCase().includes(inputValue.toLowerCase()),
         );
-        if (obj) {
-            console.log(`${obj.name} found`);
-        } else {
-            console.log(`${inputValue} not found`);
+        if (!obj) {
+            addTimeout(`${inputValue} not found`);
         }
     }
 
@@ -35,6 +40,19 @@ export default function Search({ breeds }: { breeds: Cat[] }) {
                 Search
             </button>
             <datalist id="flavors">{options}</datalist>
+            <div
+                className={`bg-white absolute ${
+                    modalMsg ? 'top-2' : '-top-1/2'
+                } left-1/2 -translate-x-1/2 border border-solid border-black p-4 transition-all duration-200`}
+            >
+                <button
+                    className="absolute top-0 right-[2px] text-xs"
+                    onClick={() => setModalMsg('')}
+                >
+                    &#x274C;
+                </button>
+                <p>{modalMsg}</p>
+            </div>
         </>
     );
 }
