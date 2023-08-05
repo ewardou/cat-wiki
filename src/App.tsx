00c213a7f8f8d0394245e169f3fcb025bd2044e8
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-08-05 13:21:32 -0400

    Modify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import { useState, useEffect } from 'react';
-import Search from './Components/SearchBar';
 import { Cat } from './types';
+import { Outlet } from 'react-router-dom';
 
 function App() {
     const [breeds, setBreeds] = useState<Cat[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
 
     async function getBreeds() {
-        const response = await fetch(
-            'https://cat-wiki-ou88.onrender.com/breeds',
-        );
-        const json: Cat[] = await response.json();
-        setBreeds(json);
-        console.log('information fetched');
+        try {
+            const response = await fetch(
+                'https://cat-wiki-ou88.onrender.com/breeds',
+            );
+            const json: Cat[] = await response.json();
+            setBreeds(json);
+            setLoading(false);
+        } catch {
+            setError(true);
+        }
     }
 
     useEffect(() => {
         getBreeds();
     }, []);
 
-    return <Search breeds={breeds} />;
+    if (error)
+        return (
+            <div className="h-screen w-screen justify-center items-center flex flex-col gap-3">
+                <h1 className="text-lg">
+                    A network error has ocurred. Try reloading the page.
+                </h1>
+                <img
+                    src="https://media.tenor.com/80SYQns-DF8AAAAd/stare-cat.gif"
+                    alt=""
+                    className="h-[300px] w-auto"
+                />
+            </div>
+        );
+    if (loading) return <h1>Loading...</h1>;
+
+    return <Outlet context={breeds} />;
 }
 
 export default App;
